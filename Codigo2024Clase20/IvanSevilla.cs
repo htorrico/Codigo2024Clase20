04b176bc4f9559888ac9216b603abe5e8899293c
Ivan Sevilla <maueth515@gmail.com>
2024-10-18 20:30:52 -0500

    multiplicar

--- Codigo2024Clase20/IvanSevilla.cs
+++ Codigo2024Clase20/IvanSevilla.cs
@@ -17,5 +17,10 @@ namespace Codigo2024Clase20
         {
             return a - b;
         }
+
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
     }
 }
