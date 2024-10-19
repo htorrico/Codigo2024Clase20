b69e5d56fdbf0d36e3e4f09b39ab19bf946ed3e4
DANTE DELOW PAEZ MANTARI <dpaezmantari@gmail.com>
2024-10-18 19:39:04 -0500

    pana

---
+++ Codigo2024Clase20/LoboTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codigo2024Clase20
+{
+    public class LoboTest
+    {
+        public void intento()
+        {
+            //intnto
+        }
+    }
+}
