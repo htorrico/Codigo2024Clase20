98ee748e62e42b07b6ae6aaad3eb595cdcf553b5
Hugo Torrico <torricomarquezh@gmail.com>
2024-10-18 22:18:59 -0500

    Calcular4taCategoria

---
+++ Codigo2024Clase20/Impuestos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codigo2024Clase20
+{
+    public class Impuestos
+    {
+        void Calcular4taCategoria()
+        {
+
+        }
+    }
+}
