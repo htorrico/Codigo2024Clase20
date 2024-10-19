8de2917f9725c7ab889add4744113319d476a5d6
Nelson Nina <nelson.np20@gmail.com>
2024-10-18 20:13:16 -0500

    update-nelos

---
+++ Codigo2024Clase20/NelsonNina.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codigo2024Clase20
+{
+	public class NelsonNina
+	{
+		public double Sumar(double x, double y)
+		{
+			return x + y;
+		}
+
+		public double Restar(double x, double y)
+		{
+			return x - y;
+		}
+
+	}
+}
